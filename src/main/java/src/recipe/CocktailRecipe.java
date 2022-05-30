f287d1f16b30a7ab3dcdb0093eff181fcddf83ae
IvayloIvanov88 <100705787+IvayloIvanov88@users.noreply.github.com>
2022-05-30 10:04:49 +0300

    CocktailRecipe

---
+++ src/main/java/src/recipe/CocktailRecipe.java
@@ -0,0 +1,26 @@
+package src.recipe;
+
+public class CocktailRecipe extends Recipe{
+
+    public CocktailRecipe(String name, int yield, int prepTime) {
+        super(name, yield, prepTime);
+    }
+
+    public CocktailRecipe() {
+        super();
+    }
+
+    @Override
+    public void setPrepTime(int prepTime) {
+        if (prepTime > 10) {
+            System.err.println("The cocktail recipe should be prepared within 10 minutes");
+        } else {
+            super.setPrepTime(prepTime);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Cocktail recipe\n" + super.toString();
+    }
+}
