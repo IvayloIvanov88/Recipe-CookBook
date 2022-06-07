63393996fc0f848bdc505a1e027eae9b2947bf57
IvayloIvanov88 <100705787+IvayloIvanov88@users.noreply.github.com>
2022-06-07 17:23:01 +0300

    refactoring Demo.java

--- src/main/java/src/Demo.java
+++ src/main/java/src/Demo.java
@@ -1,11 +1,11 @@
 package src;
 
 
-import org.jetbrains.annotations.NotNull;
 import src.recipe.*;
 import src.utils.ConsoleArt;
 import src.utils.CsvOperation;
 import src.utils.RecipeOperation;
+import src.utils.User;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
@@ -13,16 +13,13 @@ import java.util.concurrent.atomic.AtomicInteger;
 import static src.utils.Menu.showOptions;
 import static src.utils.RecipeOperation.*;
 import static src.utils.RecipeOperation.getRecipeByName;
+import static src.utils.User.*;
 
 public class Demo {
 
 
     private static final String UNHIDDEN_RECIPE_PATH = "src/main/java/src/recipe.csv";
     private static final String HIDDEN_RECIPE_PATH = "src/main/java/src/hidden.csv";
-    private static final String DELIMITER = "delimiter";
-
-    private static final Scanner SCANNER = new Scanner(System.in);
-
     public static void main(String[] args) {
 
         List<Recipe> unhiddenRecipes = new ArrayList<>();
@@ -41,7 +38,7 @@ public class Demo {
         showOptions();
 
         String choose = "";
-        while (!(choose = SCANNER.nextLine()).trim().equalsIgnoreCase("exit")) {
+        while (!(choose = User.SCANNER.nextLine()).trim().equalsIgnoreCase("exit")) {
 
             switch (choose) {
                 case "1":
@@ -127,62 +124,5 @@ public class Demo {
         artGen.draw("Goodbye", 13, ANSI_GREEN + "$" + ANSI_RESET);
         artGen.draw("Bon Appetit", 13, ANSI_RED + "#" + ANSI_RESET);
 
-    }
-
-
-    public static String getUserChoose(String message) {
-        System.out.println(ANSI_RED + message + ANSI_RESET);
-        return Demo.SCANNER.nextLine();
-    }
-
-    @NotNull
-    public static String[] getUsersChooseFileToAdd(String recipeName, int voteCount, double rating) {
-        StringBuilder sb = new StringBuilder();
-
-        sb.append(recipeName).append(DELIMITER);
-
-        System.out.println("How many portions ?");
-        String toAppendYield = Demo.SCANNER.nextLine();
-        sb.append(toAppendYield).append(DELIMITER);
-
-        System.out.println("Preparation time ?");
-        String toAppendPreparationTime = Demo.SCANNER.nextLine();
-        sb.append(toAppendPreparationTime).append(DELIMITER);
-
-        System.out.println("What are ingredients ?");
-        String toAppendIngredients = Demo.SCANNER.nextLine();
-        sb.append(toAppendIngredients).append(DELIMITER);
-
-        System.out.println("What are the preparation steps ?");
-
-        String toAppendThirdStep = Demo.SCANNER.nextLine();
-        sb.append(toAppendThirdStep).append(DELIMITER);
-
-        if (rating == 0 && voteCount == 0) {
-            String toAppendDefaultRating = "0";
-            sb.append(toAppendDefaultRating).append(DELIMITER);
-
-            String toAppendDefaultVoteCount = "0";
-            sb.append(toAppendDefaultVoteCount).append(DELIMITER);
-        } else {
-            String toAppendRating = String.valueOf(rating);
-            sb.append(toAppendRating).append(DELIMITER);
-
-            String toAppendVoteCount = String.valueOf(voteCount);
-            sb.append(toAppendVoteCount).append(DELIMITER);
-        }
-        return sb.toString().split(DELIMITER);
-
-    }
-
-    public static boolean validateUserAge(String userChoose) {
-
-        try {
-            int age = Integer.parseInt(userChoose);
-            return age >= 18;
-        } catch (NumberFormatException e) {
-            System.err.println("Enter age in digits.");
-        }
-        return false;
     }
 }
