2cfc4d55fe6919cc34870a3a26553192b2569422
Ivan P <pashalievivan@gmail.com>
2022-06-09 18:36:25 +0300

    Authentication update

--- src/main/java/src/services/MenuService.java
+++ src/main/java/src/services/MenuService.java
@@ -11,7 +11,7 @@ public class MenuService {
 
     public static final String PRINT_LINE = "|----------------------------------------------|\n";
 
-    public static void showOptions() {
+    public static void showOptions(String age) {
         System.out.println(PRINT_LINE +
                 "|\t\t" + ANSI_RED + "Welcome to Experian`s recipe book !" + ANSI_RESET + "    |\n" +
                 PRINT_LINE +
@@ -26,10 +26,21 @@ public class MenuService {
                 "|\t5. Find specific recipe by name            |\n" +
                 PRINT_LINE +
                 "|\t6. Find specific recipe by type            |\n" +
+                appendHiddenRecipes(age) +
                 PRINT_LINE + PRINT_LINE +
                 "|\tTo exit type: exit                         |\n" +
                 PRINT_LINE);
 
+
+    }
+    public static String appendHiddenRecipes(String userAge){
+        boolean giveAccess = (Integer.parseInt(userAge) > 18);
+        if (giveAccess){
+            return  PRINT_LINE +
+                    "|\t7. View hidden recipes                     |\n";
+        }else{
+            return "";
+        }
 
     }
     public static void showLoginOptions(){
